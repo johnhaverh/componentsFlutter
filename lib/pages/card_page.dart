ae36342795a57a4d6e0e25143aacd795ec77135b
John Haver Herrera <johnhaverh@gmail.com>
2022-02-07 15:56:30 -0500

    componentsflutter: cardpage: update

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,3 +1,4 @@
+import 'package:componentsflutter/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CardPage extends StatelessWidget {
@@ -6,10 +7,26 @@ class CardPage extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('CardPage'),
+    return  Scaffold(
+      appBar: AppBar(
+          title: const Text('Card Page'),
       ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        children: [
+          Card(
+            child: Column(
+              children: const [
+                ListTile(
+                  leading: Icon(Icons.photo_album_outlined,color: AppTheme.primary),
+                  title: Text('Title'),
+                  subtitle: Text('Enim sint incididunt magna enim pariatur aliquip dolor minim fugiat laborum minim. Magna sunt cupidatat ut eiusmod aute ea. Proident elit ea fugiat non dolore tempor. Reprehenderit voluptate consectetur non ullamco eiusmod ex minim consectetur minim. Et Lorem et consequat dolor'),
+                )
+              ],
+            ),
+          )
+        ],
+      )
     );
   }
 }
