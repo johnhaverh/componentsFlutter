30538f118f89951da2295236c96f3f94551a0d81
John Haver Herrera <johnhaverh@gmail.com>
2022-02-07 18:33:17 -0500

    componentsflutter: custom_card_type_2.dart & card_page.dart: update

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -15,7 +15,11 @@ class CardPage extends StatelessWidget {
         children: [
           CustomCardType1(),
           SizedBox(height: 10,),
-          CustomCardType2(),
+          CustomCardType2(name: 'A nice place', imageUrl: 'https://img.big.dk/wp-content/uploads/2020/04/ESCR_Image-by-Lumion_01.jpg'),
+          SizedBox(height: 10,),
+          CustomCardType2(name:'Hola',imageUrl: 'https://www.webopt.com/images/igallery/resized/701-800/jplenio_SDIM6992_big-775-4000-4000-100.jpg'),
+          SizedBox(height: 10,),
+          CustomCardType2(imageUrl: 'https://llandscapes-10674.kxcdn.com/wp-content/uploads/2017/04/1.jpg'),
         ],
       )
     );
--- lib/widgets/custum_card_type_2.dart
+++ lib/widgets/custum_card_type_2.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:componentsflutter/themes/app_theme.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({Key key}) : super(key: key);
+
+  final String imageUrl;
+  final String name;
+
+  const CustomCardType2({Key key, @required this.imageUrl, this.name}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,9 +19,8 @@ class CustomCardType2 extends StatelessWidget {
       shadowColor: AppTheme.primary.withOpacity(0.5),
       child: Column(
         children: [
-          
           FadeInImage(
-            image: NetworkImage('https://img.big.dk/wp-content/uploads/2020/04/ESCR_Image-by-Lumion_01.jpg'), 
+            image: NetworkImage(imageUrl), 
             placeholder: AssetImage('assets/jar-loading.gif'),
             width: double.infinity,
             height: 230,
@@ -25,11 +28,12 @@ class CustomCardType2 extends StatelessWidget {
             fadeInDuration: Duration(milliseconds: 300),
           ),
 
-          Container(
-            alignment: AlignmentDirectional.centerEnd,
-            padding: EdgeInsets.only(right: 20, top: 10, bottom: 10),
-            child: Text ('A beautiful sunset')
-          ),
+          if (name != null)
+            Container(
+              alignment: AlignmentDirectional.centerEnd,
+              padding: EdgeInsets.only(right: 20, top: 10, bottom: 10),
+              child: Text (name ?? 'No description')
+            ),
           //Image(image: NetworkImage('https://img.big.dk/wp-content/uploads/2020/04/ESCR_Image-by-Lumion_01.jpg')),
         ],
       ),
