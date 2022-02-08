ef1ab53345d9ed8d6ac0205cc5c86edaa3609b17
John Haver Herrera <johnhaverh@gmail.com>
2022-02-08 18:19:40 -0500

    componentsflutter: listview_builder_page.dart: update infiniti Scroll

--- lib/pages/listview_builder.dart
+++ lib/pages/listview_builder.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
-class ListviewBuilderPage extends StatelessWidget {
+class ListviewBuilderPage extends StatefulWidget {
    
 
   const ListviewBuilderPage({Key key}) : super(key: key);
-  
+
+  @override
+  _ListviewBuilderPageState createState() => _ListviewBuilderPageState();
+}
+
+class _ListviewBuilderPageState extends State<ListviewBuilderPage> {
+
+  final List<int> imagesIds = [1,2,3,4,5,6,7,8,9,10];
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      //print('${scrollController.position.pixels},  ${scrollController.position.maxScrollExtent}');
+      if(scrollController.position.pixels + 500>=scrollController.position.maxScrollExtent){
+        addImages();
+      }
+    });
+  }
+
+  void addImages(){
+    final lastId = imagesIds.last;
+    imagesIds.addAll(
+      [1,2,3,4,5].map((e)=> lastId + e)
+    );
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,14 +42,16 @@ class ListviewBuilderPage extends StatelessWidget {
         removeTop: true,
         removeBottom: true,
         child: ListView.builder(
-          itemCount: 10,
+          physics: BouncingScrollPhysics(),
+          itemCount: imagesIds.length,
+          controller: scrollController,
           itemBuilder: (BuildContext context, int index) {
             return FadeInImage(
               width: double.infinity,
               height: 300,
               fit: BoxFit.cover,
               placeholder: AssetImage('assets/jar-loading.gif'), 
-              image: NetworkImage('https://picsum.photos/500/300?image=${index +1}'),
+              image: NetworkImage('https://picsum.photos/500/300?image=${imagesIds[index]}'),
             );
           },
         ),
